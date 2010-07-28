bd292b004f35f2d0f07cdef8ae0de56dd4200dc7
d3f3nd3r <devnull@localhost>
2010-07-28 11:04:07 +0200

    add add_permission_to_group and add_user_to_group functions^

--- djangotoolbox/contrib/auth/utils.py
+++ djangotoolbox/contrib/auth/utils.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Permission
-from djangotoolbox.contrib.auth.models import UserPermissionList, GroupPermissionList
+from djangotoolbox.contrib.auth.models import UserPermissionList, GroupPermissionList, GroupList
 
-def add_permission_to_user(user, perm):
+def add_permission_to_user(perm, user):
     try:
         perm_list = UserPermissionList.objects.get(user=user)
     except UserPermissionList.DoesNotExist:
@@ -9,3 +9,22 @@ def add_permission_to_user(user, perm):
 
     perm_list._permission_list.append(perm.id)
     perm_list.save()
+
+
+def add_user_to_group(user, group):
+    try:
+        group_list = GroupList.objects.get(user=user)
+    except GroupList.DoesNotExist:
+        group_list = GroupList.objects.create(user=user)
+
+    group_list._group_list.append(group.id)
+    group_list.save()
+
+def add_permission_to_group(perm, group):
+    try:
+        perm_list = GroupPermissionList.objects.get(group=group)
+    except GroupPermissionList.DoesNotExist:
+        perm_list = GroupPermissionList.objects.create(group=group)
+        
+    perm_list._permission_list.append(perm.id)
+    perm_list.save()          
