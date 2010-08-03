491dfdc6b4e02db37ff807d0d41ce9f7216150e3
d3f3nd3r <devnull@localhost>
2010-08-03 22:04:30 +0200

    add upadte_permissions_user function (accepts a list of permissions)

--- djangotoolbox/auth/utils.py
+++ djangotoolbox/auth/utils.py
@@ -15,3 +15,21 @@ def add_user_to_group(user, group):
 def add_permission_to_group(perm, group):
     add_to(perm, GroupPermissionList, {'group': group})
 
+def update_permissions_user(perms, user):
+    obj_list, created = UserPermissionList.objects.get_or_create(user=user)
+
+    old_permissions = list(obj_list.permissions)
+    
+    for perm in old_permissions:
+        try:
+            perms.index(perm)
+        except ValueError:
+            obj_list.fk_list.remove(perm.id)
+    
+    for perm in perms      :
+        try:
+            old_permissions.index(perm)
+        except ValueError:
+            obj_list.fk_list.append(perm.id)
+    
+    obj_list.save()
