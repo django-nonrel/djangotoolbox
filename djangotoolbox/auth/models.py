484fead33139995badac3c48d303c0f5ffe65636
fhahn <devnull@localhost>
2010-08-28 16:13:30 +0200

    remove obsolete method get_objs()

--- djangotoolbox/auth/models.py
+++ djangotoolbox/auth/models.py
@@ -3,13 +3,6 @@ from django.db import models
 from djangotoolbox.fields import ListField
 
 
-def get_objs(obj_cls, obj_ids):
-    objs = set()
-    if len(obj_ids) > 0:
-        objs.update(obj_cls .objects.filter(id__in=obj_ids).order_by('name'))
-    return objs
-
-
 class UserPermissionList(models.Model):
     user = models.ForeignKey(User)
     
@@ -17,9 +10,6 @@ class UserPermissionList(models.Model):
     permission_fk_list = ListField(models.CharField(max_length=32))
 
     group_fk_list = ListField(models.CharField(max_length=32))
-
-    def _get_objs(self):
-        return get_objs(Group, self.group_fk_list)
 
 
 class GroupPermissionList(models.Model):
