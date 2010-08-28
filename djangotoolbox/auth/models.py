8cf19076898069c25683e49b6e62d4b89f154864
fhahn <devnull@localhost>
2010-08-28 19:51:04 +0200

    reduce permission_list CharField length to 64

--- djangotoolbox/auth/models.py
+++ djangotoolbox/auth/models.py
@@ -6,7 +6,7 @@ from djangotoolbox.fields import ListField
 class UserPermissionList(models.Model):
     user = models.ForeignKey(User)
     
-    permission_list = ListField(models.CharField(max_length=128))
+    permission_list = ListField(models.CharField(max_length=64))
     permission_fk_list = ListField(models.CharField(max_length=32))
 
     group_fk_list = ListField(models.CharField(max_length=32))
@@ -14,5 +14,5 @@ class UserPermissionList(models.Model):
 
 class GroupPermissionList(models.Model):
     group = models.ForeignKey(Group)
-    permission_list = ListField(models.CharField(max_length=128))
+    permission_list = ListField(models.CharField(max_length=64))
     permission_fk_list = ListField(models.CharField(max_length=32))
